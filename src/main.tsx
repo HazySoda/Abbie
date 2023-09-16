155bf76e021bc24a408621ae320cd48fbbceaa4a
HazySoda <wildnode@gmail.com>
2023-09-16 15:13:36 +0800

    feat(*): initial commit

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { HappyProvider } from "@ant-design/happy-work-theme";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <HappyProvider>
+      <App />
+    </HappyProvider>
+  </React.StrictMode>
+);
